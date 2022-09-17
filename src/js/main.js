91cd5498011dc779d5e1d813702b333e8ae8b93e
Diego <romerodiegoa08@gmail.com>
2022-09-17 17:21:09 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,11 @@ themeButton.addEventListener('click', () => {
         themeButton.classList.toggle('fa-moon');
     }
 })
+
+// contact link
+
+const contactIcon = document.querySelector('.contact-icon');
+
+contactIcon.onclick = () => {
+    window.open("https://github.com/iDieg", "_blank");
+}
